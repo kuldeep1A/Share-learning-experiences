f34968ab3f2cfa0d450cc928fafa1cbe92ec2c88
KULDEEP <90776144+kuldeep1A@users.noreply.github.com>
2023-05-12 18:53:25 +0530

    Add authentication and authorization

--- workex/experiences/views.py
+++ workex/experiences/views.py
@@ -1,5 +1,9 @@
 from django.shortcuts import render
 from .models import Experience
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.views.generic.edit import CreateView
+from django.urls import reverse_lazy
 
 
 def experiences_list(request):
@@ -10,3 +14,14 @@ def experiences_list(request):
 def experience_detail(request, pk):
     experience = Experience.get_constraints(pk)
     return render(request, 'experiences/experience_detail.html', {'experience': experience})
+
+
+@login_required
+class ExperienceCreate(LoginRequiredMixin, CreateView):
+    model = Experience
+    fields = ['title', 'description', 'image', 'video']
+    success_url = reverse_lazy('experience_list')
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
